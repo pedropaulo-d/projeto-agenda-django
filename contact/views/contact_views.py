b6fcdd1b9ba9e83753b071b024993b6b69b017a4
Pedro Paulo <pedrobrpp5@gmail.com>
2023-10-26 11:00:06 -0400

    Filtrando os valores com Q e OR para o campo Search e usando request no template para o valor de GET.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 def index(request):
 
@@ -9,6 +10,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -17,14 +19,46 @@ def index(request):
         context
     )
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+                
+                ) \
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value, 
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
 def contact(request, contact_id):
     
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
@@ -32,3 +66,4 @@ def contact(request, contact_id):
         'contact/contact.html',
         context
     )
+
