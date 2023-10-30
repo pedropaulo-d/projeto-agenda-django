b101bd0d8805110a8974ac4b08ccc725a59a79a8
Pedro Paulo <pedrobrpp5@gmail.com>
2023-10-30 13:31:21 -0400

    Criado formulário utilizando Forms do Django

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,21 @@
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
-from contact.models import Contact
+from django.shortcuts import render
+from contact.forms import ContactForm
+
 
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+        
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
     context = {
-
-
+        'form': ContactForm()
     }
     
     return render(
