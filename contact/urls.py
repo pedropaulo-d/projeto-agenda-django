72ffcafe4dd1d199e89050afd4149200228ff794
Pedro Paulo <pedrobrpp5@gmail.com>
2023-11-06 12:24:20 -0400

    Criado messagens para o usuário, login e logout de usuário

--- contact/urls.py
+++ contact/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
+    
+    #user
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
 ]
